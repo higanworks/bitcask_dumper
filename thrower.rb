b57654079208c89512bd9dcad1e8dfeafb55609e
sawanoboly <sawanoboriyu@higanworks.com>
2012-07-02 13:45:53 +0900

    throw to riak
    but no error handle..

--- thrower.rb
+++ thrower.rb
@@ -14,9 +14,15 @@ require 'riak'
 BASETIME = Time.now
 BACK_DIR = "./dump/" + BASETIME.strftime("%Y-%m-%dT%H%M")
 
+## define expire date
+# > 60 * 60 * 24 * 60 #=> 5184000
+EXPIRE = 5184000
+C_EXPIRE = BASETIME.to_i - EXPIRE
+
+
 ### change STDOUT to logfile
 if !ENV["DEBUG"] then
-  File.open("./log/" + BASETIME.strftime("%Y-%m-%dT%H%M") + "_dumpout.log", "a") do |file|
+  File.open("./log/" + BASETIME.strftime("%Y-%m-%dT%H%M") + "_throw.log", "a") do |file|
     STDOUT.reopen(file)
   end
 end
@@ -24,8 +30,10 @@ end
 @bits_root = "/var/db/riak/bitcask/"
 # @bits_root = "/Users/sawanoboriyu/github/local/bitcask_dumper/bits/bitcask/"
 
+# setup riak client
+riak = Riak::Client.new
 
-def dumpall()
+def throw_to_riak()
   Dir.entries(@bits_root).each do |bit_dir|
     ### skip . and ..
     next if bit_dir =~ /^\./
@@ -39,15 +47,27 @@ def dumpall()
     
       bucket, key = BERT.decode key
       value = BERT.decode value
-    
-      puts bucket + "/" + key
-      # dump the entire value to a file for later inspection.
-      FileUtils.mkdir_p(File.join(BACK_DIR,bucket))
-      File.open(File.join(BACK_DIR, bucket, key), 'w') do |out|
-        out.write value.to_json
+
+
+      # exclude expired data
+      if bucket =~ /_net[0-9]$/ then
+        if key.to_i / 1000 < C_EXPIRE
+          # log to 
+          puts "expired data : " + bucket + "/" + key
+          next
+        end
       end
+
+    # throw to riak
+    puts "throw to riak: " + bucket + "/" + key
+    ob = riak.bucket(bucket)
+    o = ob.get_or_new(key)
+    o.raw_data = value.last
+    o.content_type = "application/json"
+    o.store
+
     end
   end
 end
 
-dumpall
+throw_to_riak()
