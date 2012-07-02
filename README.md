f0a02c4ee3e6afd2e427940fe975b168b2c85290
sawanoboly <sawanoboriyu@higanworks.com>
2012-07-02 13:21:36 +0900

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 dump all buckets, keys and raw value data from riak backend storage.
 
+## Usage
+
+#### dump
+Dump all record to ./dump/ directory.
+    bundle install
+    ruby ./dumpall.rb
+
--- dumpall.rb
+++ dumpall.rb
@@ -10,29 +10,42 @@ require 'bitcask'
 require 'bert'
 require 'riak'
 
+# CONST
+BASETIME = Time.now
+BACK_DIR = "./dump/" + BASETIME.strftime("%Y-%m-%dT%H%M")
 
+### change STDOUT to logfile
+File.open("./log/" + BASETIME.strftime("%Y-%m-%dT%H%M") + "_dumpout.log", "a") do |file|
+  STDOUT.reopen(file)
+end
+
+# @bits_root = "/var/db/riak/bitcask/"
 @bits_root = "/Users/sawanoboriyu/github/local/bitcask_dumper/bits/bitcask/"
 
 
-Dir.entries(@bits_root).each do |bit_dir|
-  ### skip . and ..
-  next if bit_dir =~ /^\./
-
-  ### load bitcask
-  b = Bitcask.new File.join(@bits_root, bit_dir)
-  b.load
+def dumpall()
+  Dir.entries(@bits_root).each do |bit_dir|
+    ### skip . and ..
+    next if bit_dir =~ /^\./
   
-  b.each do |key, value|
-    next if value == Bitcask::TOMBSTONE
-  
-    bucket, key = BERT.decode key
-    value = BERT.decode value
-  
-    puts bucket + "/" + key
-    # dump the entire value to a file for later inspection.
-    # FileUtils.mkdir_p(File.join(BACK_DIR,bucket))
-    # File.open(File.join(BACK_DIR, bucket, key), 'w') do |out|
-    #   out.write value.to_json
-    # end
+    ### load bitcask
+    b = Bitcask.new File.join(@bits_root, bit_dir)
+    b.load
+    
+    b.each do |key, value|
+      next if value == Bitcask::TOMBSTONE
+    
+      bucket, key = BERT.decode key
+      value = BERT.decode value
+    
+      puts bucket + "/" + key
+      # dump the entire value to a file for later inspection.
+      FileUtils.mkdir_p(File.join(BACK_DIR,bucket))
+      File.open(File.join(BACK_DIR, bucket, key), 'w') do |out|
+        out.write value.to_json
+      end
+    end
   end
 end
+
+dumpall
